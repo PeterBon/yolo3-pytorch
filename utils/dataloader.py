8304f882472b3c1f68e72d3ba4aba40537921fd3
Peter Liu <liufei881225@outlook.com>
2020-09-19 15:01:09 +0800

    add random_perspective()

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -170,27 +170,122 @@ def random_crop(image, bboxes):
         crop_bboxes:裁剪后的bounding box的坐标array
     """
     # ---------------------- 裁剪图像 ----------------------
-    h, w, _ = image.shape
+    if len(bboxes > 0):
+        h, w, _ = image.shape
+        # 找到刚好包含所有box的框
+        max_bbox = np.concatenate([np.min(bboxes[:, 0:2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
 
-    # 没有bboxes则直接返回
-    if len(bboxes == 0):
-        return image, bboxes
-    # 找到刚好包含所有box的框
-    max_bbox = np.concatenate([np.min(bboxes[:, 0:2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
+        max_l_trans = max_bbox[0]  # 包含所有目标框的最小框到左边的距离
+        max_u_trans = max_bbox[1]  # 包含所有目标框的最小框到顶端的距离
+        max_r_trans = w - max_bbox[2]  # 包含所有目标框的最小框到右边的距离
+        max_d_trans = h - max_bbox[3]  # 包含所有目标框的最小框到底部的距离
 
-    max_l_trans = max_bbox[0]  # 包含所有目标框的最小框到左边的距离
-    max_u_trans = max_bbox[1]  # 包含所有目标框的最小框到顶端的距离
-    max_r_trans = w - max_bbox[2]  # 包含所有目标框的最小框到右边的距离
-    max_d_trans = h - max_bbox[3]  # 包含所有目标框的最小框到底部的距离
+        crop_xmin = max(0, int(max_bbox[0] - random.uniform(0, max_l_trans)))
+        crop_ymin = max(0, int(max_bbox[1] - random.uniform(0, max_u_trans)))
+        crop_xmax = max(w, int(max_bbox[2] + random.uniform(0, max_r_trans)))
+        crop_ymax = max(h, int(max_bbox[3] + random.uniform(0, max_d_trans)))
 
-    crop_xmin = max(0, int(max_bbox[0] - random.uniform(0, max_l_trans)))
-    crop_ymin = max(0, int(max_bbox[1] - random.uniform(0, max_u_trans)))
-    crop_xmax = max(w, int(max_bbox[2] + random.uniform(0, max_r_trans)))
-    crop_ymax = max(h, int(max_bbox[3] + random.uniform(0, max_d_trans)))
+        image = image[crop_ymin: crop_ymax, crop_xmin: crop_xmax]
 
-    image = image[crop_ymin: crop_ymax, crop_xmin: crop_xmax]
-
-    bboxes[:, [0, 2]] = bboxes[:, [0, 2]] - crop_xmin
-    bboxes[:, [1, 3]] = bboxes[:, [1, 3]] - crop_ymin
+        bboxes[:, [0, 2]] = bboxes[:, [0, 2]] - crop_xmin
+        bboxes[:, [1, 3]] = bboxes[:, [1, 3]] - crop_ymin
 
     return image, bboxes
+
+
+def random_perspective(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10, perspective=0.0, border=(0, 0)):
+    # torchvision.transforms.RandomAffine(degrees=(-10, 10), translate=(.1, .1), scale=(.9, 1.1), shear=(-10, 10))
+    # targets = [cls, xyxy]
+    """
+    透视变换：旋转，平移，缩放，错切，透视
+    targets = [xmin,ymin,xmax,ymax] shape = (n,4)
+    """
+    height = img.shape[0] + border[0] * 2  # shape(h,w,c)
+    width = img.shape[1] + border[1] * 2
+
+    # Center
+    C = np.eye(3)
+    C[0, 2] = -img.shape[1] / 2  # x translation (pixels)
+    C[1, 2] = -img.shape[0] / 2  # y translation (pixels)
+
+    # Perspective
+    P = np.eye(3)
+    P[2, 0] = random.uniform(-perspective, perspective)  # x perspective (about y)
+    P[2, 1] = random.uniform(-perspective, perspective)  # y perspective (about x)
+
+    # Rotation and Scale
+    R = np.eye(3)
+    a = random.uniform(-degrees, degrees)
+    # a += random.choice([-180, -90, 0, 90])  # add 90deg rotations to small rotations
+    s = random.uniform(1 - scale, 1 + scale)
+    # s = 2 ** random.uniform(-scale, scale)
+    R[:2] = cv2.getRotationMatrix2D(angle=a, center=(0, 0), scale=s)
+
+    # Shear
+    S = np.eye(3)
+    S[0, 1] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # x shear (deg)
+    S[1, 0] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # y shear (deg)
+
+    # Translation
+    T = np.eye(3)
+    T[0, 2] = random.uniform(0.5 - translate, 0.5 + translate) * width  # x translation (pixels)
+    T[1, 2] = random.uniform(0.5 - translate, 0.5 + translate) * height  # y translation (pixels)
+
+    # Combined rotation matrix
+    M = T @ S @ R @ P @ C  # order of operations (right to left) is IMPORTANT
+    if (border[0] != 0) or (border[1] != 0) or (M != np.eye(3)).any():  # image changed
+        if perspective:
+            img = cv2.warpPerspective(img, M, dsize=(width, height), borderValue=(114, 114, 114))
+        else:  # affine
+            img = cv2.warpAffine(img, M[:2], dsize=(width, height), borderValue=(114, 114, 114))
+
+    # Visualize
+    # import matplotlib.pyplot as plt
+    # ax = plt.subplots(1, 2, figsize=(12, 6))[1].ravel()
+    # ax[0].imshow(img[:, :, ::-1])  # base
+    # ax[1].imshow(img2[:, :, ::-1])  # warped
+
+    # Transform label coordinates
+    n = len(targets)
+    if n:
+        # warp points
+        xy = np.ones((n * 4, 3))
+        xy[:, :2] = targets[:, [1, 2, 3, 4, 1, 4, 3, 2]].reshape(n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
+        xy = xy @ M.T  # transform
+        if perspective:
+            xy = (xy[:, :2] / xy[:, 2:3]).reshape(n, 8)  # rescale
+        else:  # affine
+            xy = xy[:, :2].reshape(n, 8)
+
+        # create new boxes
+        x = xy[:, [0, 2, 4, 6]]
+        y = xy[:, [1, 3, 5, 7]]
+        xy = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
+
+        # # apply angle-based reduction of bounding boxes
+        # radians = a * math.pi / 180
+        # reduction = max(abs(math.sin(radians)), abs(math.cos(radians))) ** 0.5
+        # x = (xy[:, 2] + xy[:, 0]) / 2
+        # y = (xy[:, 3] + xy[:, 1]) / 2
+        # w = (xy[:, 2] - xy[:, 0]) * reduction
+        # h = (xy[:, 3] - xy[:, 1]) * reduction
+        # xy = np.concatenate((x - w / 2, y - h / 2, x + w / 2, y + h / 2)).reshape(4, n).T
+
+        # clip boxes
+        xy[:, [0, 2]] = xy[:, [0, 2]].clip(0, width)
+        xy[:, [1, 3]] = xy[:, [1, 3]].clip(0, height)
+
+        # filter candidates
+        i = box_candidates(box1=targets[:, 1:5].T * s, box2=xy.T)
+        targets = targets[i]
+        targets[:, 1:5] = xy[i]
+
+    return img, targets
+
+
+def box_candidates(box1, box2, wh_thr=2, ar_thr=20, area_thr=0.1):  # box1(4,n), box2(4,n)
+    # Compute candidate boxes: box1 before augment, box2 after augment, wh_thr (pixels), aspect_ratio_thr, area_ratio
+    w1, h1 = box1[2] - box1[0], box1[3] - box1[1]
+    w2, h2 = box2[2] - box2[0], box2[3] - box2[1]
+    ar = np.maximum(w2 / (h2 + 1e-16), h2 / (w2 + 1e-16))  # aspect ratio
+    return (w2 > wh_thr) & (h2 > wh_thr) & (w2 * h2 / (w1 * h1 + 1e-16) > area_thr) & (ar < ar_thr)  # candidates
